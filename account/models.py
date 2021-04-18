61294867cab2d87d1a0b5488e7116d0c25a44ce3
Anis Hamal Blanco <anishamal4real@gmail.com>
2021-04-18 13:51:25 +0545

    Version 1

---
+++ account/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.db import models
+from django.db.models import Sum
+
+# Create your models here.
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    house_no = models.CharField(max_length=200,null=True)
+    room_rent = models.FloatField()
+    electricity = models.FloatField()
+    wifi = models.FloatField()     
+    def __str__(self):
+        return self.name
+
+class Landlord(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=200, null=True)
+    house_no = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.name
+
+class Rent(models.Model):
+    tenant=models.ForeignKey(Tenant, null=True, on_delete=models.SET_NULL)
+    landlord=models.ForeignKey(Landlord, null=True, on_delete=models.SET_NULL)
+    date_created= models.DateTimeField(auto_now_add=True)
+
+    
+
+
+
+ 
+
+# Create your models here.
---
+++ account/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm
+from .models import *
+from django.contrib.auth.decorators import login_required
+
+
+'''
+def home(request):
+    return HttpResponse('This is the home page.')
+def tenant(request):
+    return HttpResponse('This is the page for the tenants.')
+def landlord(request):
+    return HttpResponse('This is the page for the landlords.')
+'''
+# Create your views here.
+
+def home(request):
+    return render(request, 'account/dashboard.html')
+
+def tenant(request):
+    tenants=Tenant.objects.all()
+    return render(request, 'account/tenant.html',{'tenants': tenants})
+
+def landlord(request):
+    landlords= Landlord.objects.all()
+    return render(request,'account/landlord.html', {'landlords': landlords})
+
+def registerPage(request):
+	form= CreateUserForm()
+	if request.method=='POST':
+		form= CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	context={'form':form}
+	return render(request,'account/register.html',context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user= authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request,user)
+				return redirect('register')
+			else:
+				messages.info(request, 'Username Or password is incorrect')
+		context={}
+		return render(request, 'account/login.html',context)
+    
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
