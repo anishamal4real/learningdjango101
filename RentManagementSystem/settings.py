86d4c286f15402cf51ed3fc20a502e27e4e2dba7
Anis Hamal Blanco <anishamal4real@gmail.com>
2021-04-29 10:29:22 +0545

    changes in db

--- RentManagementSystem/settings.py
+++ RentManagementSystem/settings.py
@@ -77,9 +77,15 @@ WSGI_APPLICATION = 'RentManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD':'realmadrid12345',
+        'HOST':'localhost',
+        'PORT': '5432',
+
     }
+
 }
 
 
--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [path('', views.home, name="home"),
-                path('tenant/<str:pk_test>/', views.tenant),
-                path('landlord/<str:pk>/', views.landlord),
-                path('rent/<str:pk>/', views.rent),
+                path('tenant/<str:pk_test>/', views.tenant, name="tenant"),
+                path('landlord/<str:pk>/', views.landlord,  name="landlord"),
+                path('rent/<str:pk>/', views.rent, name="rent"),
                path('register/', views.registerPage, name="register"),
                path('login/', views.loginPage, name="login"),
                path('logout/', views.logoutUser, name="logout"),
