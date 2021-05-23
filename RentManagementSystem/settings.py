c9c46ae1e039f63c753f7001cd26645b49f629e5
Anis Hamal Blanco <anishamal4real@gmail.com>
2021-05-23 20:17:46 +0545

    Added privacy

--- RentManagementSystem/settings.py
+++ RentManagementSystem/settings.py
@@ -76,13 +76,26 @@ WSGI_APPLICATION = 'RentManagementSystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import json
+import os
+from django.core.exceptions import ImproperlyConfigured
 
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+SECRET_KEY = get_secret('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
         'USER': 'postgres',
-        'PASSWORD':'realmadrid12345',
+        'PASSWORD':get_secret('DB_PASSWORD'),
         'HOST':'localhost',
         'PORT': '5432',
 
